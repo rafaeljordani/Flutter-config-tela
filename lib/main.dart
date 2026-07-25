dc5d2df7fe3731fe1366dbc64b84b9d8e2816600
Rafael Jordani Coelho <rafitapc08@gmail.com>
2026-07-24 21:00:45 -0300

    feture/Tela-finaliazada

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +34,7 @@ class MainApp extends StatelessWidget {
         ),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -81,6 +83,230 @@ class MainApp extends StatelessWidget {
                     ],
                   ),
                 ),
+
+                Container(
+                  margin: const EdgeInsets.only(bottom: 12),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+
+                  decoration: BoxDecoration(
+                    color: const Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                      color: const Color(0xffE0E0E0),
+                      width: 1,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Icon(Icons.lock, color: colorGreen, size: 32),
+                      ),
+                      const Text(
+                        'Privacidade e segurança',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xff1F1F1F),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  margin: const EdgeInsets.only(bottom: 12),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+
+                  decoration: BoxDecoration(
+                    color: const Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                      color: const Color(0xffE0E0E0),
+                      width: 1,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Icon(Icons.help, color: colorGreen, size: 32),
+                      ),
+                      const Text(
+                        'Ajuda e suporte',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xff1F1F1F),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffFFFFFF),
+                    border: Border.all(color: const Color(0xffE0E0E0)),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                child: Icon(
+                                  Icons.workspace_premium,
+                                  color: colorGreen,
+                                  size: 32,
+                                ),
+                              ),
+                              const Text(
+                                'Plano Premium',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xff1F1F1F),
+                                ),
+                              ),
+                              const Spacer(),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 4,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xffEBF5EE),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                  'Ativo',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600,
+                                    color: colorGreen,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'Armazenamento ultilizado',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Color(0xffBABABA),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 12,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFf8f8f7),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.devices,
+                                      color: colorGreen,
+                                      size: 32,
+                                    ),
+                                    const Text(
+                                      '3',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                        color: Color(0xff1F1F1F),
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Dispositivos',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Color(0xffBABABA),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFf8f8f7),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.calendar_today,
+                                      color: colorGreen,
+                                      size: 32,
+                                    ),
+                                    const Text(
+                                      '12 dias',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                        color: Color(0xff1F1F1F),
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Expira em ',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Color(0xffBABABA),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const Spacer(),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(),
+                    onPressed: olamundo,
+                    child: Text(
+                      'Sair da conta',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.red.shade300,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -103,3 +329,5 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+void olamundo() {}
